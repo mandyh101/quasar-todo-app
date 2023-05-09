5f50b38091ac7ed3032413ed0d1e88ec2ba50295
mandy <mandyhale10@gmail.com>
2023-05-09 19:34:06 +1200

    add fake paras

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,6 +1,18 @@
 <template>
   <q-page class="q-pa-lg">
     <div class="text-h4">About</div>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore,
+      molestias labore repellendus eos nostrum sed cupiditate non quam
+      necessitatibus quasi quod eum quae quisquam! Laboriosam eligendi earum
+      mollitia? Dolor, tempora.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem est
+      laborum qui accusantium nulla temporibus maiores dolorem error quia
+      tempore assumenda voluptatum, quos deleniti nam earum dicta repudiandae
+      commodi iusto?
+    </p>
   </q-page>
 </template>
 
